7be0bd316ae2fe100cd855e30659b446d752de04
Nimalnix <145547043+Nimalnix@users.noreply.github.com>
2024-01-07 15:01:22 +0530

    Add files via upload

---
+++ redux/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Counter from './features/counter/Counter';
+
+function App() {
+  return (
+    <main>
+      <Counter/>
+    </main>
+  
+  )
+}
+
+export default App;
